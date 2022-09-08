10d71a1eb6744f84adbf85ec782c93f545f6e152
lixiang <271896700@qq.com>
2022-09-08 16:56:42 +0800

    列表页基本框架

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <view>
-
+  <view style="display: flex;flex-direction: column;align-items: center;">
+    <view class="listCell" v-for="(item,index) in list" :key="index">
+    </view>
   </view>
 </template>
 
@@ -8,7 +9,7 @@
   export default {
     data() {
       return {
-        list: []
+        list: [1, 2, 3]
       }
     },
     onLoad() {
@@ -20,6 +21,21 @@
   }
 </script>
 
-<style>
+<style lang="scss">
+  page {
+    padding-bottom: 50px;
+  }
 
+  .listCell {
+    background-color: white;
+    width: 84vw;
+    border-radius: 10px;
+    padding-left: 4vw;
+    padding-right: 4vw;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    box-shadow: 0px 0px 4px 4px rgba(0, 0, 0, 0.04);
+    margin-top: 16px;
+    min-height: 40px;
+  }
 </style>
