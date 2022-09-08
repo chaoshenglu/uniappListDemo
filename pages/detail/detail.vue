8a43dc1d3eea177e27f7f55ebe3720b410d242f2
lixiang <271896700@qq.com>
2022-09-08 17:52:41 +0800

    1

---
+++ pages/detail/detail.vue
@@ -0,0 +1,25 @@
+<template>
+  <view>
+
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        id: null
+      }
+    },
+    onLoad(option) {
+      this.id = option.id
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
